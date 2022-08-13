c5fcd666d3afd261f5ce38f8dc2c138fc811bcff
Alberto Elorza <albertoelorza2002@gmail.com>
2022-08-13 19:35:35 +0200

    use the injected function return as response

--- injector.go
+++ injector.go
@@ -37,6 +37,36 @@ func validateOutput(val reflect.Type) bool {
 	}
 }
 
+/*
+This function must only be called once the function output has been validated
+*/
+func getOutputFunction(val reflect.Type) func([]reflect.Value, *gin.Context) {
+	switch val.NumOut() {
+	case 1:
+		if val.Out(0).Kind() == reflect.Int {
+			return func(out []reflect.Value, cnt *gin.Context) {
+				cnt.Status(int(out[0].Int()))
+			}
+		} else {
+			return func(out []reflect.Value, cnt *gin.Context) {
+				cnt.JSON(200, out[0].Interface())
+			}
+		}
+	case 2:
+		if val.Out(0).Kind() == reflect.Int {
+			return func(out []reflect.Value, cnt *gin.Context) {
+				cnt.JSON(int(out[0].Int()), out[1].Interface())
+			}
+		} else {
+			return func(out []reflect.Value, cnt *gin.Context) {
+				cnt.JSON(int(out[1].Int()), out[0].Interface())
+			}
+		}
+	default:
+		return nil
+	}
+}
+
 func injectFunction(val reflect.Value) gin.HandlerFunc {
 	tp := val.Type()
 	if !validateInput(tp) {
@@ -45,9 +75,12 @@ func injectFunction(val reflect.Value) gin.HandlerFunc {
 	if !validateOutput(tp) {
 		panic("Invalid output from endpoint handler")
 	}
+	useOutput := getOutputFunction(tp)
 	return func(c *gin.Context) {
-		//TODO: Use return type
-		val.Call(constructInput(c, tp.In, tp.NumIn()))
+		useOutput(
+			val.Call(constructInput(c, tp.In, tp.NumIn())),
+			c,
+		)
 	}
 }
 
